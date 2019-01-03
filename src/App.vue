895a69d8a24ac3335df3b5f0dbe12e5e77d7ab0e
Robert Zielinski <zielinskirob95@gmail.com>
2019-01-03 12:40:33 +0100

    added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,24 @@
 <template>
-  <div id="app">
-    <app-header></app-header>
-    <router-view></router-view>
-  </div>
+  <v-app>
+    <v-content>
+      <app-header></app-header>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 import Header from './components/Header.vue'
 
 export default {
+  name: 'App',
   components: {
     appHeader: Header
-  }
-}
-</script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+  },
+  data () {
+    return {
+      //
     }
   }
 }
-</style>
+</script>
